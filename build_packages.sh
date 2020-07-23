0cc55539c166dfe586d79ce9ea66f2cdd47606da
François Kooman <fkooman@tuxed.net>
2020-07-23 22:07:12 +0200

    add php-constant-time

--- build_packages.sh
+++ build_packages.sh
@@ -7,9 +7,13 @@ rm -rf ${BUILD_DIR}
 mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR}
 
-for REPO in https://git.tuxed.net/deb/php-fkooman-secookie.deb https://git.tuxed.net/deb/php-saml-sp.deb; do
+for REPO in \
+    https://salsa.debian.org/php-team/pear/php-constant-time.git \
+    https://git.tuxed.net/deb/php-fkooman-secookie.deb \
+    https://git.tuxed.net/deb/php-saml-sp.deb;
+do
 	git clone ${REPO}
-	cd $(basename ${REPO})
+	cd "$(basename ${REPO})"
 	uscan --download-current-version
 	pdebuild
 done
