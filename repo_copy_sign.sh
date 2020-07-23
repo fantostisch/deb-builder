4fda33299c560f0f63e767f295cf58cf28ab1025
François Kooman <fkooman@tuxed.net>
2020-07-23 22:18:39 +0200

    --yes

--- repo_copy_sign.sh
+++ repo_copy_sign.sh
@@ -9,5 +9,5 @@ cd ${REPO_DIR}
 /usr/bin/apt-ftparchive release . > Release
 /usr/bin/apt-ftparchive sources . > Sources 2>/dev/null
 
-gpg --clear-sign -a -o InRelease Release
-gpg --detach-sign -a -o Release.gpg Release
+gpg --yes --clear-sign -a -o InRelease Release
+gpg --yes --detach-sign -a -o Release.gpg Release
