cb4c3263f8c4ba8f37a6371a2035e29b557bda65
François Kooman <fkooman@tuxed.net>
2020-08-05 20:15:37 +0200

    initial reprepro stuff

---
+++ setup_repo.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+REPO_DIR=/var/www/repo
+
+sudo mkdir -p ${REPO_DIR}/conf
+sudo cp reprepro_distributions ${REPO_DIR}/conf/distributions
+
+# fix permissions
+sudo chown -R $(id -u -n).$(id -g -n) ${REPO_DIR}
+
+# init repo
+reprepro -b ${REPO_DIR} createsymlinks
+reprepro -b ${REPO_DIR} export
