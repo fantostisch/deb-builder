92f7ec169162fea95fcb0b03ea65fef808e60007
François Kooman <fkooman@tuxed.net>
2020-08-21 13:33:32 +0200

    write archive in HOME

--- copy_to_archive.sh
+++ copy_to_archive.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-TARGET_FILE="${PWD}/repo-$(date +%Y%m%d%H%M%S).tar.xz"
+TARGET_FILE="${HOME}/repo-$(date +%Y%m%d%H%M%S).tar.xz"
 (
 	cd /var/www/repo || exit 1
 	tar --exclude ./db --exclude ./conf -cJf "${TARGET_FILE}" .
