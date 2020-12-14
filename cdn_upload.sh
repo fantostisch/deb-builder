8fca2048bf2879cd890527973e44a9ff80d812a4
François Kooman <fkooman@tuxed.net>
2020-12-14 14:14:17 +0100

    also upload key

--- cdn_upload.sh
+++ cdn_upload.sh
@@ -5,5 +5,5 @@ SERVER_LIST="eduvpn-repo@tromso-cdn.eduroam.no eduvpn-repo@ifi2-cdn.eduroam.no"
 for SERVER in ${SERVER_LIST}; do
 	echo "${SERVER}..."
         # Debian
-	rsync -e ssh -rltO --delete /var/www/repo/dists /var/www/repo/pool "${SERVER}:/srv/repo.eduvpn.org/www/v2/deb" || echo "FAIL ${SERVER}"
+	rsync -e ssh -rltO --delete /var/www/repo/*.key /var/www/repo/dists /var/www/repo/pool "${SERVER}:/srv/repo.eduvpn.org/www/v2/deb" || echo "FAIL ${SERVER}"
 done
